1a6824dadc10b4881edf22bd44fd3cae431e08dc
lqz.leo <lqz.leo@gmail.com>
2011-05-16 09:39:19 +0000

    add publish Qi.Web project.

--- src/build.bat
+++ src/build.bat
@@ -2,5 +2,5 @@
 msbuild "%cd%\Qi\Qi\Qi.csproj"   /property:Configuration=Debug /p:Configuration=Debug /p:OutputPath="%cd%\Publish\Debug"
 msbuild "%cd%\Qi\Qi\Qi.csproj"   /property:Configuration=Release /p:Configuration=Release /p:OutputPath="%cd%\Publish\Release"
 
-msbuild "%cd%\Qi\Qi.Web.csproj"   /property:Configuration=Debug /p:Configuration=Debug /p:OutputPath="%cd%\Publish\Debug"
-msbuild "%cd%\Qi\Qi.Web.csproj"   /property:Configuration=Release /p:Configuration=Release /p:OutputPath="%cd%\Publish\Release"
+msbuild "%cd%\Qi\Qi.Web\Qi.Web.csproj"   /property:Configuration=Debug /p:Configuration=Debug /p:OutputPath="%cd%\Publish\Debug"
+msbuild "%cd%\Qi\Qi.Web\Qi.Web.csproj"   /property:Configuration=Release /p:Configuration=Release /p:OutputPath="%cd%\Publish\Release"
