721afe1099127698627db068a097c901f75e3731
lqz.leo <lqz.leo@gmail.com>
2011-05-25 08:31:36 +0000

    add Serialization and Deserialization for xml.

--- src/Qi/Qi/IO/Serialization/SerializationHelper.cs
+++ src/Qi/Qi/IO/Serialization/SerializationHelper.cs
@@ -1,12 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
 namespace Qi.IO.Serialization
 {
     public static class SerializationHelper
     {
-
         public static void SerializeBinary(this object obj, Stream outputSteam)
         {
             IFormatter formatter = new BinaryFormatter();
@@ -20,6 +21,7 @@ namespace Qi.IO.Serialization
                 obj.SerializeBinary(stream);
             }
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -29,8 +31,9 @@ namespace Qi.IO.Serialization
         public static T DeserializeBinary<T>(Stream objectStream)
         {
             IFormatter formatter = new BinaryFormatter();
-            return (T)formatter.Deserialize(objectStream);
+            return (T) formatter.Deserialize(objectStream);
         }
+
         /// <summary>
         /// 
         /// </summary>
@@ -47,6 +50,42 @@ namespace Qi.IO.Serialization
             {
                 return DeserializeBinary<T>(stream);
             }
+        }
+
+        public static void SerializerXml(this object obj, Stream outputSteam)
+        {
+            var ser = new XmlSerializer(obj.GetType());
+            ser.Serialize(outputSteam, obj);
+        }
+
+        public static void SerializerXml(this object obj, string file)
+        {
+            var ser = new XmlSerializer(obj.GetType());
+            using (var stream = new FileStream(file, FileMode.OpenOrCreate))
+            {
+                ser.Serialize(stream, obj);
+            }
+        }
+
+        public static object DeserializerXml(string file, Type type)
+        {
+            if (!File.Exists(file))
+                throw new FileNotFoundException("File not found.", file);
+            using (var stream = new FileStream(file, FileMode.Open))
+            {
+                return DeserializerXml(stream, type);
+            }
+        }
+
+        public static object DeserializerXml(Stream stream, Type type)
+        {
+            var ser = new XmlSerializer(type);
+            return ser.Deserialize(stream);
+        }
+
+        public static T DeserializerXml<T>(string file)
+        {
+            return (T) DeserializerXml(file, typeof (T));
         }
     }
 }
