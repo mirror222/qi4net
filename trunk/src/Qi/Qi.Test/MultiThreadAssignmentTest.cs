8511711d2808a9ff375229dbb78d0eac804e0ad8
lqz.leo@gmail.com <lqz.leo@gmail.com@aced55bd-fe50-1b8b-16d8-a92f41972ad9>
2011-11-14 10:11:54 +0000

    fix bug on Threads/Assignments/IAssignment.cs

---
+++ trunk/src/Qi/Qi.Test/MultiThreadAssignmentTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Qi.Threads;
+
+namespace Qi.Test
+{
+    [TestClass]
+    public class MultiThreadAssignmentTest
+    {
+        [TestMethod]
+        public void TestCase_sum_0_100_in_synre()
+        {
+            var sumData = new int[100];
+            var actual = 0;
+            for (int i = 0; i < sumData.Length; i++)
+            {
+                sumData[i] = i;
+                actual += i;
+            }
+            var result = 0;
+            MultiThreadAssignment<int> MultiSummary = new MultiThreadAssignment<int>(5, s => result += s.Sum());
+            MultiSummary.Execute(sumData);
+            Assert.AreEqual(actual, result);
+        }
+
+
+
+        [TestMethod]
+        public void TestCase_sum_0_100_in_asynre()
+        {
+            var sumData = new int[100];
+            var actual = 0;
+            for (int i = 0; i < sumData.Length; i++)
+            {
+                sumData[i] = i;
+                actual += i;
+            }
+            var result = 0;
+            MultiThreadAssignment<int> MultiSummary = new MultiThreadAssignment<int>(5, s => result += s.Sum());
+            MultiSummary.Execute(sumData);
+            Assert.AreEqual(actual, result);
+        }
+
+        [TestMethod]
+        public void TestCase_sum_0_to_3_use_5_thread_on_setting_in_synre()
+        {
+            var sumData = new int[3];
+            var actual = 0;
+            for (int i = 0; i < sumData.Length; i++)
+            {
+                sumData[i] = i;
+                actual += i;
+            }
+            var result = 0;
+            MultiThreadAssignment<int> MultiSummary = new MultiThreadAssignment<int>(5, s => result += s.Sum());
+            MultiSummary.Execute(sumData);
+            Assert.AreEqual(actual, result);
+        }
+
+      
+        
+    }
+}
--- trunk/src/Qi/Qi/Threads/MultiThreadAssignment.cs
+++ trunk/src/Qi/Qi/Threads/MultiThreadAssignment.cs
@@ -4,58 +4,77 @@ using System.Threading;
 
 namespace Qi.Threads
 {
-    public delegate void ExecutionHandler(object[] data);
+    public delegate void ExecutionHandler<T>(IList<T> data);
 
-    public sealed class MultiThreadAssignment
+
+    public sealed class MultiThreadAssignment<T>
     {
-        private readonly ExecutionHandler _executeHandler;
+        private readonly ExecutionHandler<T> _executeHandler;
         private readonly int _threadCount;
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="threadCount">启用多少个Thread去执行,最大9999,最少1</param>
         /// <param name="threadState"></param>
         /// <exception cref="ArgumentOutOfRangeException">当<see cref="threadCount"/>不在1～9999范围之内</exception>
-        public MultiThreadAssignment(int threadCount, ExecutionHandler threadState)
+        public MultiThreadAssignment(int threadCount, ExecutionHandler<T> threadState)
         {
             if (threadCount < 0 || threadCount > 9999)
-                throw new ArgumentOutOfRangeException("threadCount", "Should be between 1~20");
+                throw new ArgumentOutOfRangeException("threadCount", "Should be between 1~9999");
             if (threadState == null)
                 throw new ArgumentNullException("threadState");
             _threadCount = threadCount;
             _executeHandler = threadState;
         }
-        
-        public void Execute(object[] datas)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="datas"></param>
+        public void Execute(IList<T> datas)
         {
-            if (datas.Length == 0)
+            if (datas == null)
+                throw new ArgumentNullException("datas");
+            if (datas.Count == 0)
                 return;
-            int threadCount = _threadCount < datas.Length ? _threadCount : datas.Length;
-            var threads = new Thread[threadCount];
-            var executeData = new List<List<object>>();
-            int i = 0;
-            foreach (object obj in datas)
+            IAsyncResult i = AsyncExecute(datas, null);
+            WaitHandle t = i.AsyncWaitHandle;
+            t.WaitOne();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="datas"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public IAsyncResult AsyncExecute(IList<T> datas, AsyncCallback callback)
+        {
+            var waitCallBack = new WaitCallback(Waitting);
+            if (datas.Count == 0)
             {
-                //平均分配到每一个Thread和data
-                if (i >= _threadCount)
-                    i = 0;
-                if (threads[i] == null)
-                {
-                    threads[i] = new Thread(state => _executeHandler((object[])state));
-                }
-                if (executeData.Count <= i)
-                {
-                    executeData.Add(new List<object>());
-                }
-                executeData[i].Add(obj);
-                i++;
+                return waitCallBack.BeginInvoke(new Thread[0], callback, null);
             }
 
-            for (int j = 0; j < threads.Length; j++)
+            IList<List<T>> assignedData = AssignThread(datas);
+            var threads = new Thread[assignedData.Count];
+            int i = 0;
+            foreach (var a in assignedData)
             {
-                threads[j].Start(executeData[j].ToArray());
+                threads[i] = new Thread(s => _executeHandler((IList<T>) s)) {IsBackground = true};
+                threads[i].Start(a);
+                i++;
             }
-         
+            return waitCallBack.BeginInvoke(threads, callback, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="state"></param>
+        private static void Waitting(object state)
+        {
+            var threads = (Thread[]) state;
             while (true)
             {
                 bool allfinish = false;
@@ -73,6 +92,32 @@ namespace Qi.Threads
                 }
                 Thread.Sleep(500);
             }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="datas"></param>
+        /// <returns></returns>
+        private IList<List<T>> AssignThread(IEnumerable<T> datas)
+        {
+            //int threadCount = _threadCount < datas.Count ? _threadCount : datas.Count;
+            var assignedDataSet = new List<List<T>>();
+            int i = 0;
+            foreach (T obj in datas)
+            {
+                //平均分配到每一个Thread和data
+                if (i >= _threadCount)
+                    i = 0;
+                if (assignedDataSet.Count <= i)
+                {
+                    assignedDataSet.Add(new List<T>());
+                }
+                assignedDataSet[i].Add(obj);
+                i++;
+            }
+
+            return assignedDataSet;
         }
     }
 }
