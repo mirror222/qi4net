92015d2f8805de45de304c8b8489d769d6ca2eba
lqz.leo <lqz.leo@gmail.com>
2011-12-27 02:06:24 +0000

    add addHelper

---
+++ src/Qi/Qi.Test/ArrayHelperTest.cs
@@ -0,0 +1,153 @@
+﻿using Qi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Qi.Test
+{
+
+
+    /// <summary>
+    ///This is a test class for ArrayHelperTest and is intended
+    ///to contain all ArrayHelperTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ArrayHelperTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Split
+        ///</summary>
+        [TestMethod]
+        public void SplitTestHelper()
+        {
+            int[] ary = new int[33];
+            for (int i = 0; i < ary.Length; i++)
+            {
+                ary[i] = i;
+            }
+            int eachAryLength = 10;
+
+            int remainder = 0;
+            int remainderExpected = 3;
+
+            var actual = ArrayHelper.Split<int>(ary, eachAryLength, out remainder);
+            Assert.AreEqual(4, actual.Count);
+            Assert.AreEqual(3, remainder);
+            Assert.AreEqual(remainderExpected, remainder);
+
+        }
+
+
+
+        [TestMethod()]
+        public void DivEqualTest_7_div_3()
+        {
+            int[] ary = new int[7];
+            for (int i = 0; i < ary.Length; i++)
+            {
+                ary[i] = i;
+            }
+            int dividEquallyNumber = 3;
+            var target = ary.DivEqual(dividEquallyNumber);
+            Assert.AreEqual(target.Length, 3);
+            Assert.AreEqual(3, target[0].Length);
+            var startChecked = 0;
+            for (int i = startChecked; i < target[0].Length; i++)
+            {
+                Assert.AreEqual(i, target[0][i]);
+                startChecked++;
+            }
+
+            Assert.AreEqual(2, target[1].Length);
+            for (int i = startChecked; i < target[1].Length; i++)
+            {
+                Assert.AreEqual(i, target[1][i]);
+            }
+        }
+
+        [TestMethod()]
+        public void DivEqualTest_8_for_4()
+        {
+            int[] ary = new int[8];
+            for (int i = 0; i < ary.Length; i++)
+            {
+                ary[i] = i;
+            }
+            int dividEquallyNumber = 4;
+            var target = ary.DivEqual(dividEquallyNumber);
+            Assert.AreEqual(target.Length, 4);
+            foreach (var aryItem in target)
+            {
+                Assert.AreEqual(2, aryItem.Length);
+            }
+        }
+
+        [TestMethod()]
+        public void DivEqualTest_4_for_8()
+        {
+            int[] ary = new int[4];
+            for (int i = 0; i < ary.Length; i++)
+            {
+                ary[i] = i;
+            }
+            int dividEquallyNumber = 8;
+            var target = ary.DivEqual(dividEquallyNumber);
+            Assert.AreEqual(target.Length, 4);
+            foreach (var aryItem in target)
+            {
+                Assert.AreEqual(1, aryItem.Length);
+            }
+        }
+    }
+}
---
+++ src/Qi/Qi/ArrayHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Qi
+{
+    public static class ArrayHelper
+    {
+        /// <summary>
+        /// 把ary 按照每份拥有多个来分组数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ary"></param>
+        /// <param name="maxLengthOfAry"></param>
+        /// <returns></returns>
+        public static IList<T[]> Split<T>(this T[] ary, int maxLengthOfAry)
+        {
+            int remider;
+            return ary.Split(maxLengthOfAry, out remider);
+        }
+
+        /// <summary>
+        /// 把Ary按照每分多少个Item进行切割
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ary"></param>
+        /// <param name="maxLengthOfAry">每个数组的最大个数</param>
+        /// <param name="remainder"></param>
+        /// <returns></returns>
+        public static IList<T[]> Split<T>(this T[] ary, int maxLengthOfAry, out int remainder)
+        {
+            if (ary == null)
+                throw new ArgumentNullException("ary");
+            if (ary.Length == 0)
+            {
+                remainder = 0;
+                return new List<T[]>();
+            }
+
+            int arySetLen = Math.DivRem(ary.Length, maxLengthOfAry, out remainder);
+            var arySet = new List<T[]>(arySetLen + remainder);
+            if (arySetLen != 0)
+            {
+                for (int i = 0; i < arySetLen; i++)
+                {
+                    var aryItems = new T[maxLengthOfAry];
+                    Array.Copy(ary, maxLengthOfAry*i, aryItems, 0, maxLengthOfAry);
+                    arySet.Add(aryItems);
+                }
+            }
+            if (remainder != 0)
+            {
+                var aryItems = new T[remainder];
+                Array.Copy(ary, maxLengthOfAry*arySetLen, aryItems, 0, remainder);
+                arySet.Add(aryItems);
+            }
+            return arySet;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ary"></param>
+        /// <param name="dividEquallyNumber"></param>
+        /// <returns></returns>
+        public static T[][] DivEqual<T>(this T[] ary, int dividEquallyNumber)
+        {
+            if (ary == null)
+            {
+                throw new ArgumentNullException("ary");
+            }
+            if (ary.Length == 0)
+            {
+                var r = new T[0][];
+                r[0] = new T[0];
+                return r;
+            }
+            if (dividEquallyNumber <= 0)
+            {
+                throw new ArgumentOutOfRangeException("dividEquallyNumber", "dividEquallyNumber should be more than 0.");
+            }
+
+            var result = new T[dividEquallyNumber < ary.Length ? dividEquallyNumber : ary.Length][];
+
+            int remainder;
+            int arylength = Math.DivRem(ary.Length, dividEquallyNumber, out remainder);
+            int pos = 0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                int aryLength = remainder == 0 ? arylength : arylength + 1;
+                var aryItem = new T[aryLength];
+                result[i] = aryItem;
+                Array.Copy(ary, pos, aryItem, 0, aryLength);
+                if (remainder != 0)
+                {
+                    remainder--;
+                }
+                pos += aryItem.Length;
+            }
+            return result;
+        }
+    }
+}
