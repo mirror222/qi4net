37ae881ff03a201da14dcbc8fe92d909c30a2c54
lqz.leo@gmail.com <lqz.leo@gmail.com@aced55bd-fe50-1b8b-16d8-a92f41972ad9>
2011-12-28 02:31:11 +0000

    refactory name-space

---
+++ trunk/src/Qi/Qi/Secret/Sha1Helper.cs
@@ -0,0 +1,121 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Qi.Secret
+{
+    public static class Sha1Helper
+    {
+        #region Sha1
+
+        /// <summary>
+        ///用sha1 utf8进行加密
+        /// </summary>
+        public static byte[] Sha1Utf8(this string content)
+        {
+            return Sha(content, Encoding.UTF8, new SHA1CryptoServiceProvider());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha1ASCII(this string content)
+        {
+            return Sha(content, Encoding.ASCII, new SHA1CryptoServiceProvider());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha1Unicode(this string content)
+        {
+            return Sha(content, Encoding.Unicode, new SHA1CryptoServiceProvider());
+        }
+
+        #endregion
+
+        #region sha384
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha384Utf8(this string content)
+        {
+            return Sha(content, Encoding.UTF8, new SHA384CryptoServiceProvider());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha384ASCII(this string content)
+        {
+            return Sha(content, Encoding.ASCII, new SHA384CryptoServiceProvider());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha384Unicode(this string content)
+        {
+            return Sha(content, Encoding.Unicode, new SHA384CryptoServiceProvider());
+        }
+
+        #endregion
+
+        #region sha512
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha512Utf8(this string content)
+        {
+            return Sha(content, Encoding.UTF8, new SHA512CryptoServiceProvider());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha512ASCII(this string content)
+        {
+            return Sha(content, Encoding.ASCII, new SHA512CryptoServiceProvider());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static byte[] Sha512Unicode(this string content)
+        {
+            return Sha(content, Encoding.Unicode, new SHA512CryptoServiceProvider());
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content">加密的方法</param>
+        /// <param name="getBytesFunc">把content转换为byte的方法</param>
+        /// <param name="hashAlgorithm">加密的方式</param>
+        /// <returns></returns>
+        private static byte[] Sha(string content, Encoding getBytesFunc, HashAlgorithm hashAlgorithm)
+        {
+            byte[] iput = getBytesFunc.GetBytes(content);
+            return hashAlgorithm.ComputeHash(iput);
+        }
+    }
+}
--- trunk/src/Qi/ThreadPerformerTest/Program.cs
+++ trunk/src/Qi/ThreadPerformerTest/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using Qi.Text;
+using Qi.Secret;
 using Qi.Threads;
 
 namespace ThreadPerformerTest
