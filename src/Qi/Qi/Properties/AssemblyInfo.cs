8f222c33cd539639e96146cdfb3eb566bccd3c95
lqz.leo <lqz.leo@gmail.com>
2011-12-29 05:52:24 +0000

    change the version.

--- src/Qi/Qi/Properties/AssemblyInfo.cs
+++ src/Qi/Qi/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("Qi")]
-[assembly: AssemblyDescription("Nothing")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Qi")]
 [assembly: AssemblyProduct("Qi")]
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
