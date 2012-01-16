3ef50a84a3f64a45492a3949cd5cf43c711bc452
lqz.leo <lqz.leo@gmail.com>
2012-01-16 04:12:48 +0000

    Add initalization.

---
+++ src/Qi/Qi/LazyInitialization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qi
+{
+    public class LazyInitialization
+    {
+        private readonly Dictionary<int, object> _inits = new Dictionary<int, object>();
+        /// <summary>
+        /// Init once and return the new one.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="initMethod"></param>
+        /// <returns></returns>
+        public T Once<T>(Func<T> initMethod)
+        {
+            var key = initMethod.GetHashCode();
+            if (!_inits.ContainsKey(key))
+            {
+                _inits.Add(key, initMethod());
+            }
+            return (T)_inits[key];
+        }
+        
+    }
+}
